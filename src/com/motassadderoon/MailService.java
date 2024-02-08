6bc1a1e60037ce12f211d1c93136c707503ad386
codiaji <codiaji@gmail.com>
2024-02-08 16:51:19 +0000

    Abstraction

---
+++ src/com/motassadderoon/MailService.java
@@ -0,0 +1,22 @@
+package com.motassadderoon;
+
+public class MailService {
+    public void sendEmail() {
+        connect();
+        authenticate();
+        // send email
+        disconnect();
+    }
+
+    private void connect() {
+
+    }
+
+    private void disconnect() {
+
+    }
+
+    private void authenticate() {
+
+    }
+}
--- src/com/motassadderoon/Main.java
+++ src/com/motassadderoon/Main.java
@@ -2,11 +2,8 @@ package com.motassadderoon;
 
 public class Main {
     public static void main(String[] args) {
-        Account myAcc = new Account();
-        myAcc.deposit(10);
-        myAcc.deposit(20);
-        myAcc.withDraw(15);
-        System.out.println("myAcc.getBalance() = " + myAcc.getBalance());
+        MailService mailService = new MailService();
+        mailService.sendEmail();
     }
 
     public static TaxCalculator getTaxCalculator() {
